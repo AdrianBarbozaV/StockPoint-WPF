197650a6ac27804e0f9881a93a26b4f1daf68b2a
Adrian Barboza <adribarboza2004@gmail.com>
2026-06-28 21:08:29 -0600

    agrego TabControl oscuro con tabs para orden y gestion de producto

---
+++ StockPoint.WPF/Views/ProductoUserControl.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace StockPoint.WPF.Views
+{
+    public partial class ProductoUserControl : UserControl
+    {
+        public ProductoUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
